d81e61589c6058cc29c59b8e85ca70ccfa0b08d3
Rifat Ahmed <ahmed1807103@stud.kuet.ac.bd>
2022-07-26 08:23:15 +0600

    New Class Added

---
+++ app/src/main/java/com/example/cardiacrecord/Records.java
@@ -0,0 +1,15 @@
+package com.example.cardiacrecord;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Records {
+    public static ArrayList<ModelClass> mcl = new ArrayList<>();
+
+    public void addRecord(ModelClass modelClass){
+        if(mcl.contains(modelClass)){
+            throw new IllegalArgumentException();
+        }
+        mcl.add(modelClass);
+    }
+}
